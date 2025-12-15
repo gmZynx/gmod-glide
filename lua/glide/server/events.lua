81959c19dc57ed62c2d31f5fba314662ba801b8c
StyledStrike <lameulucas@gmail.com>
2025-12-15 20:31:02 -0300

    Minor fixes for #237

--- lua/glide/server/events.lua
+++ lua/glide/server/events.lua
@@ -240,6 +240,28 @@ do
     end )
 end
 
+-- The spawnmenu is configured to recognize it as a vehicle,
+-- so we should run spawn hooks that are for vehicles too.
+hook.Add( "PlayerSpawnSENT", "Glide.RunVehiclePreSpawnHook", function( ply, entClass )
+    local entTable = scripted_ents.GetStored( entClass )
+    if not entTable then return end
+
+    local vehicleTable = entTable["t"]
+    if not vehicleTable or not isstring( vehicleTable["GlideCategory"] ) then return end
+
+    local bAllow = hook.Run( "PlayerSpawnVehicle", ply, vehicleTable["ChassisModel"], vehicleTable["PrintName"], vehicleTable )
+    return bAllow ~= false
+end )
+
+hook.Add( "PlayerSpawnedSENT", "Glide.RunVehiclePostSpawnHook", function( ply, ent )
+    if not IsValid( ent ) then return end
+    if not ent.IsGlideVehicle then return end
+
+    hook.Run( "PlayerSpawnedVehicle", ply, ent )
+
+    return true
+end )
+
 if not game.SinglePlayer() then return end
 
 local function ResetVehicle( vehicle )
@@ -326,26 +348,4 @@ hook.Add( "ClientSignOnStateChanged", "Glide.RestoreVehicle", function( _, _, ne
     if newState == SIGNONSTATE_FULL then
         timer.Simple( 0.2, ResetAll )
     end
-end )
-
--- The spawnmenu is configured to recognize it as a vehicle (Compatibility with all systems that require this hook, for example, a permission system that allows a vehicle to spawn)
-hook.Add( "PlayerSpawnSENT", "Glide.VehicleSpawnCheck", function( ply, entClass )
-    local entTable = scripted_ents.GetStored( entClass )
-    if not entTable then return end
-
-    local entGlide = entTable["t"]
-    if not entGlide or not isstring( entGlide["GlideCategory"] ) then return end
-
-    local bHook = hook.Run( "PlayerSpawnVehicle", ply, entTable.ChassisModel, entGlide["PrintName"], entGlide )
-
-    return bHook
-end )
-
-hook.Add( "PlayerSpawnedSENT", "Glide.VehicleSpawnSetup", function( ply, ent )
-    if not IsValid( ent ) then return end
-    if not ent.IsGlideVehicle then return end
-
-    hook.Run( "PlayerSpawnedVehicle", ply, ent )
-
-    return true
 end )
