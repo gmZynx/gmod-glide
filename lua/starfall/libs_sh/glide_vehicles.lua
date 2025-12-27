b0bf9258e00ebd2b6fb820b18fa0060b13126cd2
Mikey-Mikey <mikeygmoore07@gmail.com>
2025-12-27 00:04:27 -0500

    Adhere to cfc guidelines

--- lua/starfall/libs_sh/glide_vehicles.lua
+++ lua/starfall/libs_sh/glide_vehicles.lua
@@ -1,23 +1,23 @@
-local ENT_META = FindMetaTable("Entity")
+local ENT_META = FindMetaTable( "Entity" )
 
-return function(instance)
+return function( instance )
 
-local Ent_IsValid,Ent_IsWorld = ENT_META.IsValid,ENT_META.IsWorld
+local Ent_IsValid, Ent_IsWorld = ENT_META.IsValid, ENT_META.IsWorld
 local ents_methods, ent_meta = instance.Types.Entity.Methods, instance.Types.Entity
 
-local function getent(self)
+local function getent( self )
 	local ent = ent_meta.sf2sensitive[self]
-	if Ent_IsValid(ent) or Ent_IsWorld(ent) then
+	if Ent_IsValid( ent ) or Ent_IsWorld( ent ) then
 		return ent
 	else
-		SF.Throw("Entity is not valid.", 3)
+		SF.Throw( "Entity is not valid.", 3 )
 	end
 end
 
 --- Returns whether the entity is a glide vehicle
 -- @return boolean True if the entity is a glide vehicle
 function ents_methods:isGlideVehicle()
-	return getent(self).IsGlideVehicle and true or false
+	return getent( self ).IsGlideVehicle and true or false
 end
 
 end
